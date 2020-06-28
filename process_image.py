bca86efac494f6aa444d3f6e01e63f0834f60953
Subramanian Ramasamy <67526803+Subram0212@users.noreply.github.com>
2020-06-27 20:49:33 -0500

    Add files via upload

--- process_image.py
+++ process_image.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
-# import matplotlib.pyplot as plt
 import time
-import traversal1
+import img_traversing
 import astar_search
 from skimage.metrics import structural_similarity as ssim
 
@@ -11,7 +10,7 @@ def main(image_file):
     global result_1
     occupied_grids = []
     planned_path = {}
-
+    # Read the image
     image = cv2.imread(image_file)
     (winW, winH) = (60, 60)
 
@@ -21,9 +20,9 @@ def main(image_file):
     list_images = [[blank_image for i in range(8)] for i in range(8)]
     maze = [[0 for i in range(8)] for i in range(8)]
 
-    for(x, y, window) in traversal1.sliding_window(image, stepSize=60, windowSize=(winW, winH)):
+    # traversing through each grid blocks of the image and store the average color of each grid
+    for(x, y, window) in img_traversing.sliding_window(image, stepSize=60, windowSize=(winW, winH)):
         clone = image.copy()
-        # global clone
         cv2.rectangle(clone, (x, y), (x + winW, y+winH), (0, 0, 0), -1)
         crop_image = image[x:x+winW, y:y+winH]
         list_images[index[0]-1][index[1]-1] = crop_image.copy()
@@ -32,11 +31,12 @@ def main(image_file):
         average_color_per_box = np.average(average_color_per_row, axis=0)
         average_color_per_box =np.uint8(average_color_per_box)
 
+        # if the grid is not white completely, store the grids with coloured objects in a list
         if(any(i <= 240 for i in average_color_per_box)):
             maze[index[0]-1][index[1]-1] = 1
             occupied_grids.append(tuple(index))
 
-
+        # put those grids with black obstacles in a separate obstacles' list
         if(any(i <= 20 for i in average_color_per_box)):
             obstacles.append(tuple(index))
 
@@ -62,7 +62,9 @@ def main(image_file):
 
             s = ssim(image_1, image_2)
 
+            # if structural similarity index is greater than 0.9, those grids with same objects' shape match each other
             if s > 0.9:
+                # perform A* search
                 result = astar_search.astar(maze, (startimage[0]-1, startimage[1]-1), (grid[0]-1, grid[1]-1))
 
                 list2 = []
@@ -74,6 +76,8 @@ def main(image_file):
                         list2.append(tuple((x, y)))
                         _result = list(list2[1:-1])
                         result_1 = list(list2[:])
+
+                # join those two objects of same shape with a line
                 while result_1:
                     first_co_ord = result_1.pop(0)
                     if result_1:
@@ -85,30 +89,10 @@ def main(image_file):
                         cv2.imshow('Window', mat=image)
                         result_1.insert(0, last_co_ord)
 
-                # for t in result:
-                #     # x = t[0]
-                #     # y_ = t[1]
-                #     # cv2.line(image.shape[0], (x, y), (x + winW, y + winH), (0, 0, 0), -1)
-                #     for(x, y, window) in traversal1.sliding_window(image, stepSize=60, windowSize=(winW, winH)):
-                #         clone = image.copy()
-                #         cv2.line(clone, (x, y), (x + winW, y+winH), (0, 0, 0), -1)
-                #         cv2.imshow('Window', clone)
-
                 if not result:
                     planned_path[startimage] = list(['NO PATH', [], 0])
-                # path = np.array(result)
-                # plt.plot(path[:, 0], path[:, 1])
-                # # plt.axis('equal')
-                # # plt.xscale(1)
-                # plt.xlim(1, 10)
-                # # plt.yscale(1)
-                # # plt.axis('equal')
-                # plt.ylim(1, 10)
-                # # plt.axis('square')
-                # plt.gca().invert_yaxis()
-                # # plt.gca().set_aspect('equal', 'box')
-                # plt.grid(True)
-                # plt.show()
+
+                # store the co-ordinates of paths joining those two same objects inside a list
                 planned_path[startimage] = list([str(grid), _result, len(_result)+1])
 
     for obj in list_of_colored_grids:
@@ -126,8 +110,6 @@ if __name__ == "__main__":
     print(occupied_path)
     print("Planned path is: ")
     print(planned_path)
-
-    # cv2.line(img=clone, pt1=planned_path.keys(), pt2=planned_path.values(), color='Black', thickness=2)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
